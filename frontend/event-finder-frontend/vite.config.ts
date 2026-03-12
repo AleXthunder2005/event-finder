8d5904aedd245beafb7021c4636998ae880e32c4
AleXthunder2005 <aleksandrnar2005@gmail.com>
2026-03-12 23:46:07 +0300

    refactor: setup vite aliases, default style schema and folders structure

--- frontend/event-finder-frontend/vite.config.ts
+++ frontend/event-finder-frontend/vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +13,18 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      '@ui': path.resolve(__dirname, './src/ui'),
+      '@components': path.resolve(__dirname, './src/ui/components'),
+      '@layouts': path.resolve(__dirname, './src/ui/layouts'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@icons': path.resolve(__dirname, './assets/icons'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@images': path.resolve(__dirname, './assets/images'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@contexts': path.resolve(__dirname, './src/contexts'),
+    },
+  },
 })
